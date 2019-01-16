18108dd06b9d2f674ea9e0bfc197c0fddef18290
sspog <sspog.lim@gmail.com>
2019-01-17 00:19:27 +0900

    Update Data Crawling Program (Firebase)
    Finish Data update method (include All)
    But we need to consider "how to check new posts at cse(ref.issue#3)"

--- crawling/data_crawler_set.py
+++ crawling/data_crawler_set.py
@@ -37,7 +37,7 @@ options.add_argument('window-size=1920x1080')
 options.add_argument('disable-gpu')
 
 
-# In[6]:
+# In[4]:
 
 
 cau_title_list = []
@@ -48,28 +48,29 @@ driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 driver.get("https://www.cau.ac.kr/cms/FR_CON/index.do?MENU_ID=100")
 driver.implicitly_wait(3)
 
-#caunotice_page = driver.page_source
-#caunotice_soup = BeautifulSoup(caunotice_page,'lxml')
-
 cau_base_url = "https://www.cau.ac.kr/cms/FR_CON/BoardView.do?MENU_ID=100&CONTENTS_NO=1&SITE_NO=2&P_TAB_NO=&TAB_NO=&BOARD_SEQ=4&BOARD_CATEGORY_NO=&BBS_SEQ="
 # BBS_SEQ=19642 (id=board_19642)
 
-title_list = driver.find_elements_by_class_name("txtL")
-for item in range(len(title_list)):
-    cau_title_list.append(title_list[item].find_element_by_tag_name('a').text)
-
-url_list = driver.find_element_by_id("tbody").find_elements_by_tag_name("li")
-for item in url_list:
+board_list = driver.find_element_by_id("tbody").find_elements_by_tag_name("li")
+board_list.reverse()
+# count = 0
+for item in board_list:
+    # if count < 10: pass # 테스트용
+    # else:
+    cau_title_list.append(item.find_element_by_class_name("txtL").find_element_by_tag_name('a').text)
+    cau_date_list.append(item.find_element_by_class_name("txtInfo").find_element_by_class_name("date").text)
     cau_url_list.append(cau_base_url + item.get_attribute("id").replace("board_",""))
-    
-date_list = driver.find_elements_by_class_name("txtInfo")
-for item in range(len(date_list)):    
-    cau_date_list.append(date_list[item].find_element_by_class_name("date").text)
-
+    # count += 1
 driver.close()
 
+# list 앞에 원소를 추가할 때, insert(0,data) 사용 시 O(n)
+# class collections.deque([iterable[, maxlen]])의 dequeue() 사용 시 O(1)
+# High Performance를 원한다면 사용하자.
 
-# In[7]:
+# 혹은 list reverse 후, append 계속 사용 (단, reverse의 경우 O(n))
+
+
+# In[5]:
 
 
 lib_title_list = []
@@ -80,18 +81,11 @@ driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 driver.get("https://library.cau.ac.kr/#/bbs/notice?offset=0&max=20")
 driver.implicitly_wait(3)
 
-#제목 크롤링 부분
-#title_list = driver.find_elements_by_class_name("ikc-item-title")
-#for line in range(len(title_list)):
-#    lib_list.append(title_list[line].text)
-
-board_list = driver.find_elements_by_class_name("ikc-item")
-#회색 상단 공지 부분으로 아래 공지 중에서 중요한 것들만 올려놓은듯. 즉, 겹치는 내용임.
-#for item in board_list[0].find_elements_by_tag_name("tr"):
-#    print(item.find_elements_by_tag_name("td")[3].find_elements_by_tag_name("span")[1].text)
-
 try:
-    for item in board_list: # tbody 검색후, ikc-item 검색시, 가끔씩 IndexError: list index out of range 발생
+    # tbody[0]는 회색 상단 공지 부분으로 아래 공지 중에서 중요한 것들만 올려놓은듯. 즉, 겹치는 내용임.
+    board_list = driver.find_elements_by_tag_name("tbody")[1].find_elements_by_class_name("ikc-item")
+    board_list.reverse()
+    for item in board_list: # tbody 검색후 ikc-item 검색시, 가끔씩 IndexError: list index out of range 발생 (이유 모름)
         lib_title_list.append(item.find_elements_by_tag_name("td")[2].find_element_by_tag_name('a').text) # 대체
         lib_date_list.append(item.find_elements_by_tag_name("td")[3].find_elements_by_tag_name("span")[1].text)
 except IndexError:
@@ -101,13 +95,12 @@ except IndexError:
 lib_base_url = "https://library.cau.ac.kr/#/bbs/notice/"
 # 사이에 id 추가
 lib_sub_url = "?offset=0&max=20"
-
 # url id는 어떻게 가져올까..
 
 driver.close()
 
 
-# In[9]:
+# In[6]:
 
 
 # 노란색 공지 부분만 가져온다
@@ -119,8 +112,9 @@ dormnotice_url = "https://dormitory.cau.ac.kr/bbs/bbs_list.php?bbsID=notice"
 dormnotice_page = urlopen(dormnotice_url)
 dormnotice_soup = BeautifulSoup(dormnotice_page, "lxml")
 
-#print(dormnotice_soup.prettify())
 dormnotice_list = dormnotice_soup.find(id='content').find('div').find_all('tr',{'bgcolor':'#fffcdb'})
+dormnotice_list.reverse()
+
 if dormnotice_list == []:
     print("No data")
 else :
@@ -128,11 +122,11 @@ else :
         dorm_title_list.append(item.find('span',class_='bbsTitle').get_text())
         dorm_url_list.append(item.find('a')['href'])
         dorm_date_list.append("20" + item.find_all('td',class_='t_c')[3].get_text())
-        
+
 #try-except 적용하기?
 
 
-# In[10]:
+# In[7]:
 
 
 ict_title_list = []
@@ -148,6 +142,8 @@ ict_base_url = "http://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php?cmd=view&c
 ict_sub_url = "&search_gbn=1&search_keyword="
 
 ictnotice_list = ictnotice_soup.find('tbody').find_all('tr')
+ictnotice_list.reverse()
+
 if ictnotice_list == []:
     print("No data")
 else:
@@ -157,9 +153,10 @@ else:
         ict_date_list.append(item.find_all('td')[2].get_text())
 
 
-# In[11]:
+# In[8]:
 
 
+# 공지표시 되어있는 게시글 제목도 수집? (겹치는 내용임)
 cse_title_list = []
 cse_date_list = []
 cse_url_list = []
@@ -169,6 +166,7 @@ csenotice_page = urlopen(csenotice_url)
 csenotice_soup = BeautifulSoup(csenotice_page, "lxml")
 
 csenotice_list = csenotice_soup.find('table',class_='nlist').find_all('tr')
+csenotice_list.reverse()
 
 if csenotice_list == []:
     print("No data")
@@ -177,11 +175,9 @@ else:
         cse_title_list.append(re.sub('[\n\t\xa0]','',item.find('a').get_text())) # sub메소드 사용법 검토하기
         cse_url_list.append(csenotice_url + item.find_all('td')[2].find('a')['href'])
         cse_date_list.append(item.find_all('td')[4].get_text())
-        
-# 공지표시 되어있는 게시글 제목도 수집?
 
 
-# In[12]:
+# In[9]:
 
 
 # Firebase에 크롤링한 데이터 저장하기
@@ -199,4 +195,3 @@ crawling_data['cse'] = {'title':cse_title_list, 'date':cse_date_list, 'url':cse_
 crawling_json = json.dumps(crawling_data, ensure_ascii=False, indent="\t")
 webpage_ref = ref.child('webpages')
 webpage_ref.set(json.loads(crawling_json))
-
--- crawling/data_crawler_update.py
+++ crawling/data_crawler_update.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[13]:
+# In[1]:
 
 
 import firebase_admin
@@ -16,8 +16,10 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://cau-hashkeyword.firebaseio.com'
 })
 
+ref = db.reference('server/saving-data/crawling')
 
-# In[14]:
+
+# In[2]:
 
 
 from bs4 import BeautifulSoup
@@ -35,63 +37,110 @@ options.add_argument('window-size=1920x1080')
 options.add_argument('disable-gpu')
 
 
-# In[6]:
+# In[4]:
 
 
-cau_title_list = []
-cau_date_list = []
-cau_url_list = []
+# 추가된 게시글을 하나하나 update 하는 version1 (현재사용:x)
+import time
+start_time = time.time()
+
+ref = db.reference('server/saving-data/crawling/webpages/caunotice/url')
+cau_db_url = ref.get() # 다 가져오면 무리 아닐까. 만약 최신 게시글의 index가 더 빠르면 0으로 체크하면 돼서 필요x
+
+new_cau_title_list = []
+new_cau_date_list = []
+new_cau_url_list = []
 
 driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 driver.get("https://www.cau.ac.kr/cms/FR_CON/index.do?MENU_ID=100")
 driver.implicitly_wait(3)
 
-#caunotice_page = driver.page_source
-#caunotice_soup = BeautifulSoup(caunotice_page,'lxml')
-
 cau_base_url = "https://www.cau.ac.kr/cms/FR_CON/BoardView.do?MENU_ID=100&CONTENTS_NO=1&SITE_NO=2&P_TAB_NO=&TAB_NO=&BOARD_SEQ=4&BOARD_CATEGORY_NO=&BBS_SEQ="
 # BBS_SEQ=19642 (id=board_19642)
 
-title_list = driver.find_elements_by_class_name("txtL")
-for item in range(len(title_list)):
-    cau_title_list.append(title_list[item].find_element_by_tag_name('a').text)
+ref = db.reference('server/saving-data/crawling/webpages/caunotice')
+title_ref = ref.child('title')
+date_ref = ref.child('date')
+url_ref = ref.child('url')
 
-url_list = driver.find_element_by_id("tbody").find_elements_by_tag_name("li")
-for item in url_list:
-    cau_url_list.append(cau_base_url + item.get_attribute("id").replace("board_",""))
-    
-date_list = driver.find_elements_by_class_name("txtInfo")
-for item in range(len(date_list)):    
-    cau_date_list.append(date_list[item].find_element_by_class_name("date").text)
+length = len(cau_db_url)
+
+board_list = driver.find_element_by_id("tbody").find_elements_by_tag_name("li")
+for item in board_list:
+    if cau_db_url[length - 1] == cau_base_url + item.get_attribute("id").replace("board_",""): break
+    new_cau_title_list.insert(0, item.find_element_by_class_name("txtL").find_element_by_tag_name('a').text)
+    new_cau_date_list.insert(0, item.find_element_by_class_name("txtInfo").find_element_by_class_name("date").text)
+    new_cau_url_list.insert(0, cau_base_url + item.get_attribute("id").replace("board_",""))
+
+for index in range(len(new_cau_title_list)):
+    cau_data = OrderedDict()
+
+    cau_data[length + index] = new_cau_title_list[index]
+    cau_json = json.dumps(cau_data, ensure_ascii=False, indent="\t")
+    title_ref.update(json.loads(cau_json))
+
+    cau_data[length + index] = new_cau_date_list[index]
+    cau_json = json.dumps(cau_data, ensure_ascii=False, indent="\t")
+    date_ref.update(json.loads(cau_json))
+
+    cau_data[length + index] = new_cau_url_list[index]
+    cau_json = json.dumps(cau_data, ensure_ascii=False, indent="\t")
+    url_ref.update(json.loads(cau_json))
+
+print("--- %s seconds ---" %(time.time() - start_time))
 
 driver.close()
 
 
-# In[7]:
+# In[5]:
 
 
-lib_title_list = []
-lib_date_list = []
-lib_url_list = []
+# 모든 데이터를 한꺼번에 다시 update 하는 version2 (현재사용:o)
+# version1과 성능 및 효율성 비교해보기
+
+# 나중에 불러오는 것이 많아질 수록 느려질 것?
+ref = db.reference('server/saving-data/crawling/webpages/caunotice')
+cau_title_list = ref.child('title').get()
+cau_date_list = ref.child('date').get()
+cau_url_list = ref.child('url').get()
+
+driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
+driver.get("https://www.cau.ac.kr/cms/FR_CON/index.do?MENU_ID=100")
+driver.implicitly_wait(3)
+
+cau_base_url = "https://www.cau.ac.kr/cms/FR_CON/BoardView.do?MENU_ID=100&CONTENTS_NO=1&SITE_NO=2&P_TAB_NO=&TAB_NO=&BOARD_SEQ=4&BOARD_CATEGORY_NO=&BBS_SEQ="
+# BBS_SEQ=19642 (id=board_19642)
+
+length = len(cau_url_list)
+
+board_list = driver.find_element_by_id("tbody").find_elements_by_tag_name("li")
+for item in board_list:
+    if cau_url_list[length - 1] == cau_base_url + item.get_attribute("id").replace("board_",""): break
+    cau_title_list.insert(length, item.find_element_by_class_name("txtL").find_element_by_tag_name('a').text)
+    cau_date_list.insert(length, item.find_element_by_class_name("txtInfo").find_element_by_class_name("date").text)
+    cau_url_list.insert(length, cau_base_url + item.get_attribute("id").replace("board_",""))
+
+driver.close()
+
+
+# In[6]:
+
+
+ref = db.reference('server/saving-data/crawling/webpages/library')
+lib_title_list = ref.child('title').get()
+lib_date_list = ref.child('date').get()
 
 driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 driver.get("https://library.cau.ac.kr/#/bbs/notice?offset=0&max=20")
 driver.implicitly_wait(3)
 
-#제목 크롤링 부분
-#title_list = driver.find_elements_by_class_name("ikc-item-title")
-#for line in range(len(title_list)):
-#    lib_list.append(title_list[line].text)
-
-board_list = driver.find_elements_by_class_name("ikc-item")
-#회색 상단 공지 부분으로 아래 공지 중에서 중요한 것들만 올려놓은듯. 즉, 겹치는 내용임.
-#for item in board_list[0].find_elements_by_tag_name("tr"):
-#    print(item.find_elements_by_tag_name("td")[3].find_elements_by_tag_name("span")[1].text)
-
+length = len(lib_title_list)
 try:
-    for item in board_list: # tbody 검색후, ikc-item 검색시, 가끔씩 IndexError: list index out of range 발생
-        lib_title_list.append(item.find_elements_by_tag_name("td")[2].find_element_by_tag_name('a').text) # 대체
-        lib_date_list.append(item.find_elements_by_tag_name("td")[3].find_elements_by_tag_name("span")[1].text)
+    board_list = driver.find_elements_by_tag_name("tbody")[1].find_elements_by_class_name("ikc-item")
+    for item in board_list:
+        if lib_title_list[length - 1] == item.find_elements_by_tag_name("td")[2].find_element_by_tag_name('a').text: break
+        lib_title_list.insert(length, item.find_elements_by_tag_name("td")[2].find_element_by_tag_name('a').text)
+        lib_date_list.insert(length, item.find_elements_by_tag_name("td")[3].find_elements_by_tag_name("span")[1].text)
 except IndexError:
     print("IndexError")
     pass
@@ -105,48 +154,40 @@ lib_sub_url = "?offset=0&max=20"
 driver.close()
 
 
-# In[18]:
+# In[7]:
 
 
 # 노란색 공지 부분만 가져온다
-'''ref = db.reference('server/saving-data/crawling/webpages/dorm/title/0')
-dorm_recent_title = ref.get()
-ref = db.reference('server/saving-data/crawling/webpages/dorm/date/0')
-dorm_recent_date = ref.get()'''
-ref = db.reference('server/saving-data/crawling/webpages/dorm/url/0')
-dorm_recent_url = ref.get()
+ref = db.reference('server/saving-data/crawling/webpages/dorm')
+dorm_title_list = ref.child('title').get()
+dorm_date_list = ref.child('date').get()
+dorm_url_list = ref.child('url').get()
 
 dormnotice_url = "https://dormitory.cau.ac.kr/bbs/bbs_list.php?bbsID=notice"
 dormnotice_page = urlopen(dormnotice_url)
 dormnotice_soup = BeautifulSoup(dormnotice_page, "lxml")
 
-#print(dormnotice_soup.prettify())
 dormnotice_list = dormnotice_soup.find(id='content').find('div').find_all('tr',{'bgcolor':'#fffcdb'})
+
+length = len(dorm_url_list)
 if dormnotice_list == []:
     print("No data")
 else :
     # url로 비교하기.
     for item in dormnotice_list:
-        print(dorm_recent_url, item.find('a')['href'])
-        if dorm_recent_url == item.find('a')['href']: break
-        ref = db.reference('server/saving-data/crawling/webpages/dorm')
-        title_ref = ref.child('title')
-        date_ref = ref.child('date')
-        url_ref = ref.child('url')
-        # 데이터 update시 append? or insert(0,data)?
-        # 만약 append라면, #fecc933 commit에서 data set할 때, 순서 역방향으로 바꿔주기. -> 그러면, title/? 마지막 자리를 어떻게 아는가
-        title_ref.update(item.find('span',class_='bbsTitle').get_text())
-        date_ref.update("20" + item.find_all('td',class_='t_c')[3].get_text())
-        url_ref.update(item.find('a')['href'])
-        
-#try-except 적용하기?
+        if dorm_url_list[length - 1] == item.find('a')['href']: break
+        dorm_title_list.insert(length, item.find('span',class_='bbsTitle').get_text())
+        dorm_date_list.insert(length, "20" + item.find_all('td',class_='t_c')[3].get_text())
+        dorm_url_list.insert(length, item.find('a')['href'])
 
 
-# In[21]:
+# In[8]:
 
 
-ref = db.reference('server/saving-data/crawling/webpages/ict/url/0')
-ict_recent_url = ref.get()
+ref = db.reference('server/saving-data/crawling/webpages/ict')
+ict_title_list = ref.child('title').get()
+ict_date_list = ref.child('date').get()
+ict_url_list = ref.child('url').get()
 
 ictnotice_url = "http://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php"
 ictnotice_page = urlopen(ictnotice_url)
@@ -157,25 +198,25 @@ ict_base_url = "http://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php?cmd=view&c
 ict_sub_url = "&search_gbn=1&search_keyword="
 
 ictnotice_list = ictnotice_soup.find('tbody').find_all('tr')
+length = len(ict_url_list)
+
 if ictnotice_list == []:
     print("No data")
 else:
     for item in ictnotice_list:
-        if ict_recent_url == ict_base_url + item.find('td',class_='cont').find('a')['href'][-7:-3] + ict_sub_url: break
-        ref = db.reference('server/saving-data/crawling/webpages/ict')
-        title_ref = ref.child('title')
-        date_ref = ref.child('date')
-        url_ref = ref.child('url')
-        title_ref.update(item.find('td',class_='cont').find('a').get_text())
-        date_ref.update(item.find_all('td')[2].get_text())
-        url_ref.update(ict_base_url + item.find('td',class_='cont').find('a')['href'][-7:-3] + ict_sub_url)
+        if ict_url_list[length - 1] == ict_base_url + item.find('td',class_='cont').find('a')['href'][-7:-3] + ict_sub_url: break
+        ict_title_list.insert(length, item.find('td',class_='cont').find('a').get_text())
+        ict_date_list.insert(length, item.find_all('td')[2].get_text())
+        ict_url_list.insert(length, ict_base_url + item.find('td',class_='cont').find('a')['href'][-7:-3] + ict_sub_url)
 
 
-# In[24]:
+# In[9]:
 
 
-ref = db.reference('server/saving-data/crawling/webpages/cse/url/0')
-cse_recent_url = ref.get()
+ref = db.reference('server/saving-data/crawling/webpages/cse')
+cse_title_list = ref.child('title').get()
+cse_date_list = ref.child('date').get()
+cse_url_list = ref.child('url').get()
 
 csenotice_url = "http://cse.cau.ac.kr/20141201/sub05/sub0501.php"
 csenotice_page = urlopen(csenotice_url)
@@ -183,18 +224,33 @@ csenotice_soup = BeautifulSoup(csenotice_page, "lxml")
 
 csenotice_list = csenotice_soup.find('table',class_='nlist').find_all('tr')
 
+length = len(cse_url_list)
 if csenotice_list == []:
     print("No data")
 else:
     for item in csenotice_list:
-        if cse_recent_url == csenotice_url + item.find_all('td')[2].find('a')['href']: break
-        ref = db.reference('server/saving-data/crawling/webpages/cse')
-        title_ref = ref.child('title')
-        date_ref = ref.child('date')
-        url_ref = ref.child('url')
-        title_ref.update(re.sub('[\n\t\xa0]','',item.find('a').get_text()))
-        date_ref.update(item.find_all('td')[4].get_text())
-        url_ref.update(csenotice_url + item.find_all('td')[2].find('a')['href'])
-        
-# 공지표시 되어있는 게시글 제목도 수집?
+        if cse_url_list[length - 1] == csenotice_url + item.find_all('td')[2].find('a')['href']: break
+        # 근데 cse 페이지는 특이사항이라서, 파란 공지 부분을 비교하면 안될텐데..
+        cse_title_list.insert(length, re.sub('[\n\t\xa0]','',item.find('a').get_text()))
+        cse_date_list.insert(length, item.find_all('td')[4].get_text())
+        cse_url_list.insert(length, csenotice_url + item.find_all('td')[2].find('a')['href'])
 
+
+# In[10]:
+
+
+# Firebase에 새로 추가된 게시물 업데이트
+import json
+from collections import OrderedDict
+
+new_crawling_data = OrderedDict()
+
+new_crawling_data['caunotice'] = {'title':cau_title_list, 'date':cau_date_list, 'url':cau_url_list}
+new_crawling_data['library'] = {'title':lib_title_list, 'date':lib_date_list, 'url':"https://library.cau.ac.kr/#/bbs/notice?offset=0&max=20"}
+new_crawling_data['dorm'] = {'title':dorm_title_list, 'date':dorm_date_list, 'url':dorm_url_list}
+new_crawling_data['ict'] = {'title':ict_title_list, 'date':ict_date_list, 'url':ict_url_list}
+new_crawling_data['cse'] = {'title':cse_title_list, 'date':cse_date_list, 'url':cse_url_list}
+
+new_crawling_json = json.dumps(new_crawling_data, ensure_ascii=False, indent="\t")
+webpage_ref = ref.child('webpages')
+webpage_ref.set(json.loads(new_crawling_json))
