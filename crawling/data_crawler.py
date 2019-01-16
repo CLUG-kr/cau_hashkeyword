db2d28bfd1a1c01a82e80d29d5e502074cedb47a
sspog <sspog.lim@gmail.com>
2019-01-16 13:41:34 +0900

    Update Data Crawling Program (Firebase)
    Finish Data set method

--- crawling/data_crawler.py
+++ crawling/data_crawler.py
@@ -1,7 +1,26 @@
 
 # coding: utf-8
 
-# In[120]:
+# In[1]:
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('/Users/Solomon/Desktop/cau-hashkeyword-serviceAccountKey.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://cau-hashkeyword.firebaseio.com'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference('server/saving-data/crawling')
+
+
+# In[2]:
 
 
 from bs4 import BeautifulSoup
@@ -10,7 +29,7 @@ from selenium import webdriver
 import re
 
 
-# In[121]:
+# In[3]:
 
 
 options = webdriver.ChromeOptions()
@@ -19,12 +38,12 @@ options.add_argument('window-size=1920x1080')
 options.add_argument('disable-gpu')
 
 
-# In[131]:
+# In[6]:
 
 
-cau_list = []
-cau_url_list = []
+cau_title_list = []
 cau_date_list = []
+cau_url_list = []
 
 driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 driver.get("https://www.cau.ac.kr/cms/FR_CON/index.do?MENU_ID=100")
@@ -38,7 +57,7 @@ cau_base_url = "https://www.cau.ac.kr/cms/FR_CON/BoardView.do?MENU_ID=100&CONTEN
 
 title_list = driver.find_elements_by_class_name("txtL")
 for item in range(len(title_list)):
-    cau_list.append(title_list[item].find_element_by_tag_name('a').text)
+    cau_title_list.append(title_list[item].find_element_by_tag_name('a').text)
 
 url_list = driver.find_element_by_id("tbody").find_elements_by_tag_name("li")
 for item in url_list:
@@ -51,12 +70,12 @@ for item in range(len(date_list)):
 driver.close()
 
 
-# In[182]:
+# In[7]:
 
 
-lib_list = []
-lib_url_list = []
+lib_title_list = []
 lib_date_list = []
+lib_url_list = []
 
 driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=options)
 driver.get("https://library.cau.ac.kr/#/bbs/notice?offset=0&max=20")
@@ -74,7 +93,7 @@ board_list = driver.find_elements_by_class_name("ikc-item")
 
 try:
     for item in board_list: # tbody 검색후, ikc-item 검색시, 가끔씩 IndexError: list index out of range 발생
-        lib_list.append(item.find_elements_by_tag_name("td")[2].find_element_by_tag_name('a').text) # 대체
+        lib_title_list.append(item.find_elements_by_tag_name("td")[2].find_element_by_tag_name('a').text) # 대체
         lib_date_list.append(item.find_elements_by_tag_name("td")[3].find_elements_by_tag_name("span")[1].text)
 except IndexError:
     print("IndexError")
@@ -89,13 +108,13 @@ lib_sub_url = "?offset=0&max=20"
 driver.close()
 
 
-# In[58]:
+# In[9]:
 
 
 # 노란색 공지 부분만 가져온다
-dorm_list = []
-dorm_url_list = []
+dorm_title_list = []
 dorm_date_list = []
+dorm_url_list = []
 
 dormnotice_url = "https://dormitory.cau.ac.kr/bbs/bbs_list.php?bbsID=notice"
 dormnotice_page = urlopen(dormnotice_url)
@@ -107,19 +126,19 @@ if dormnotice_list == []:
     print("No data")
 else :
     for item in dormnotice_list:
-        dorm_list.append(item.find('span',class_='bbsTitle').get_text())
+        dorm_title_list.append(item.find('span',class_='bbsTitle').get_text())
         dorm_url_list.append(item.find('a')['href'])
-        dorm_date_list.append(item.find_all('td',class_='t_c')[3].get_text())
+        dorm_date_list.append("20" + item.find_all('td',class_='t_c')[3].get_text())
 
 #try-except 적용하기?
 
 
-# In[141]:
+# In[10]:
 
 
-ict_list = []
-ict_url_list = []
+ict_title_list = []
 ict_date_list = []
+ict_url_list = []
 
 ictnotice_url = "http://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php"
 ictnotice_page = urlopen(ictnotice_url)
@@ -134,17 +153,17 @@ if ictnotice_list == []:
     print("No data")
 else:
     for item in ictnotice_list:
-        ict_list.append(item.find('td',class_='cont').find('a').get_text())
+        ict_title_list.append(item.find('td',class_='cont').find('a').get_text())
         ict_url_list.append(ict_base_url + item.find('td',class_='cont').find('a')['href'][-7:-3] + ict_sub_url)
         ict_date_list.append(item.find_all('td')[2].get_text())
 
 
-# In[118]:
+# In[11]:
 
 
-cse_list = []
-cse_url_list = []
+cse_title_list = []
 cse_date_list = []
+cse_url_list = []
 
 csenotice_url = "http://cse.cau.ac.kr/20141201/sub05/sub0501.php"
 csenotice_page = urlopen(csenotice_url)
@@ -156,15 +175,37 @@ if csenotice_list == []:
     print("No data")
 else:
     for item in csenotice_list:
-        cse_list.append(re.sub('[\n\t\xa0]','',item.find('a').get_text())) # sub메소드 사용법 검토하기
+        cse_title_list.append(re.sub('[\n\t\xa0]','',item.find('a').get_text())) # sub메소드 사용법 검토하기
         cse_url_list.append(csenotice_url + item.find_all('td')[2].find('a')['href'])
         cse_date_list.append(item.find_all('td')[4].get_text())
 
+# 공지표시 되어있는 게시글 제목도 수집?
 
-# In[18]:
+
+# In[10]:
 
 
 result = []
 keyword = input("알림 받고자 하는 키워드를 입력해주세요! # ")
-result = [string for string in dorm_list if keyword in string]
+result = [string for string in dorm_title_list if keyword in string]
 result
+
+
+# In[12]:
+
+
+# Firebase에 크롤링한 데이터 저장하기
+import json
+from collections import OrderedDict
+
+crawling_data = OrderedDict()
+
+crawling_data['caunotice'] = {'title':cau_title_list, 'date':cau_date_list, 'url':cau_url_list}
+crawling_data['library'] = {'title':lib_title_list, 'date':lib_date_list, 'url':"https://library.cau.ac.kr/#/bbs/notice?offset=0&max=20"}
+crawling_data['dorm'] = {'title':dorm_title_list, 'date':dorm_date_list, 'url':dorm_url_list}
+crawling_data['ict'] = {'title':ict_title_list, 'date':ict_date_list, 'url':ict_url_list}
+crawling_data['cse'] = {'title':cse_title_list, 'date':cse_date_list, 'url':cse_url_list}
+
+crawling_json = json.dumps(crawling_data, ensure_ascii=False, indent="\t")
+webpage_ref = ref.child('webpages')
+webpage_ref.set(json.loads(crawling_json))
