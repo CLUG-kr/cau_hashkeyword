db8c16913a0c001883bf682a3adc5b7f32849d08
Unknown <ally3k@icloud.com>
2019-01-28 11:42:38 +0900

    Make Liststorage.swift file

---
+++ CAU_hashkeyword_IOS_application/CAU_hashkeyword_IOS_application/ListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewController.swift
+//  CAU_hashkeyword_IOS_application
+//
+//  Created by Hyeseong Kim on 28/01/2019.
+//  Copyright © 2019 Hyeseong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    var list = [ListStorage]()
+    
+    override func viewDidLoad() {
+    }
+}
