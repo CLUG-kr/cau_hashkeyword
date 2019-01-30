79428399059eb62ee23388de45c44ef604d0f034
Unknown <ally3k@icloud.com>
2019-01-31 02:58:38 +0900

    Finish TextField UI

--- CAU_hashkeyword_IOS_application/CAU_hashkeyword_IOS_application/ViewController.swift
+++ CAU_hashkeyword_IOS_application/CAU_hashkeyword_IOS_application/ViewController.swift
@@ -1,4 +1,4 @@
-//
+//DeisgnableView
 //  ViewController.swift
 //  CAU_hashkeyword_IOS_application
 //
@@ -8,20 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var viewTextField: UIView!
+    @IBOutlet var inputField: UITextField!
+    @IBOutlet var outputField: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.inputField.frame.size.width = 283.0
+        self.inputField.layer.borderWidth = 2.0
+        self.inputField.layer.cornerRadius = 3.0
+        self.inputField.layer.borderColor = UIColor.lightGray.cgColor
+        self.inputField.layer.shadowRadius = 3.0
+        self.inputField.layer.shadowColor = UIColor.black.cgColor
+        self.inputField.layer.shadowOffset = CGSize(width: 0.3, height: 0.3)
+        self.inputField.layer.shadowOpacity = 1.0
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func insert(sender: AnyObject) {
         
+        outputField.text=inputField.text;
+        inputField.text="";
+        
+    }
+    
+    func textFieldShouldReturn(_ inputField: UITextField) -> Bool {
+        outputField.text = inputField.text
+        inputField.text = ""
+        inputField.resignFirstResponder();
+        return true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    @IBAction func Setting(_ sender: Any) {
     }
 }
