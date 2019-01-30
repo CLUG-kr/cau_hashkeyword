ca5196e7aa4cbfd01e36060d5c6a7281b1ab5c8d
Unknown <ally3k@icloud.com>
2019-01-30 11:43:34 +0900

    make home navigationbar transparent

--- CAU_hashkeyword_IOS_application/CAU_hashkeyword_IOS_application/ViewController.swift
+++ CAU_hashkeyword_IOS_application/CAU_hashkeyword_IOS_application/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     @IBAction func Setting(_ sender: Any) {
     }
 }
-
 
 class DetailViewController: UIViewController, UITextFieldDelegate {
     
